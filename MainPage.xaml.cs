c690e51fcdd6fa8ce4d86963e60b9154f3ac2536
Indel <Indel@DESKTOP-BEC38OO>
2024-06-26 14:11:01 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace AlgoritmoLangosta
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(entryNP.Text))
+            {
+                double NP, pago;
+                NP = Convert.ToDouble(entryNP.Text);
+                if (NP >= 300 )
+                {
+                    pago = NP * 75;
+                }
+                else if (NP >= 200 && NP <= 300)
+                {
+                    pago = NP * 85;
+                }
+                else
+                {
+                    pago = NP * 95;
+                }
+                entryPago.Text = pago.ToString();
+            }
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AlgoritmoLangosta
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
